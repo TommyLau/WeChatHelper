283720f10204d4ce2d1f4c772ee810ee4f9dab7b
Tommy Lau <tommy@gen-new.com>
2019-10-13 03:47:36 +0800

    Basic structure

---
+++ WeChatHelper/main.mm
@@ -0,0 +1,15 @@
+//
+//  main.mm
+//  WeChatHelper
+//
+//  Created by Tommy Lau on 2019/10/13.
+//  Copyright © 2019 Tommy Lau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+__attribute__((constructor))
+static void  initialize(void) {
+    // Initialize here
+    NSLog(@"========== WeChatHelper ==========\n");
+}
