283099e948f264f0f5f1bfc7bd2164736f33ec09
Tommy Lau <tommy@gen-new.com>
2019-10-13 01:56:46 +0800

    Initial Commit

---
+++ WeChatHelper/WeChatHelper.h
@@ -0,0 +1,19 @@
+//
+//  WeChatHelper.h
+//  WeChatHelper
+//
+//  Created by Tommy Lau on 2019/10/13.
+//  Copyright © 2019 Tommy Lau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeChatHelper.
+FOUNDATION_EXPORT double WeChatHelperVersionNumber;
+
+//! Project version string for WeChatHelper.
+FOUNDATION_EXPORT const unsigned char WeChatHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeChatHelper/PublicHeader.h>
+
+
